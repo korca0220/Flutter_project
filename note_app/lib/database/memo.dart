40df8982eec4c0e7020ed45de74af0f6457f6f87
Junewoo <korca02220@naver.com>
2020-12-21 18:38:22 +0900

    SQLite DB 연동 추가 https://github.com/korca0220/Flutter_project/issues/3
    - edit onChange 추가
    - edit 'save' 버튼 클릭시 DB 저장 동작 추가

---
+++ note_app/lib/database/memo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Memo {
+  final int id;
+  final String title;
+  final String text;
+  final String create_time;
+  final String edit_time;
+
+  Memo({
+    this.id,
+    this.title,
+    this.text,
+    this.create_time,
+    this.edit_time
+  });
+
+  Map<String, dynamic> toMap(){
+    return {
+      'id' : id,
+      'title' : title,
+      'text' : text,
+      'create_time': create_time,
+      'edit_time': edit_time
+    };
+  }
+
+  @override
+  String toString(){
+    return 'Memo(id: $id, title: $title, text: $text, create_time: $create_time, edit_time:$edit_time)';
+  }
+}
--- note_app/lib/screens/edit.dart
+++ note_app/lib/screens/edit.dart
@@ -1,36 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/database/db.dart';
+import 'package:note_app/database/memo.dart';
 
 class EditPage extends StatelessWidget {
+  String title = '';
+  String text = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(icon: const Icon(Icons.delete), onPressed: () {}),
-            IconButton(icon: const Icon(Icons.save), onPressed: () {})
-          ],
-        ),
-        body: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: <Widget>[
-                TextField(
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                        hintText: '제목을 적어주세요'
-                    )
-                ),
-                Padding(padding: EdgeInsets.all(10)),
-                TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                        hintText: '메모의 내용을 적어주세요'
-                    )
-                )
-              ],
-            )));
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.delete), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.save),
+              onPressed: saveDB)
+        ],
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                  onChanged: (String title) {
+                    this.title = title;
+                  },
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      hintText: '제목을 적어주세요'
+                  )
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              TextField(
+                  onChanged: (String text) {
+                    this.text = text;
+                  },
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      hintText: '메모의 내용을 적어주세요'
+                  )
+              )
+            ],
+          )
+      ));
+  }
+  Future<void> saveDB() async {
+    DBHelper sd = DBHelper();
+
+    var fido = Memo(
+      // TODO : id 필드는 랜덤하게 생성
+        id: 1,
+        title: this.title,
+        text: this.text,
+        create_time: DateTime.now().toString(),
+        edit_time: DateTime.now().toString()
+    );
+
+    await sd.insertMemo(fido);
+
+    // Log 확인용
+    print(await sd.memos());
   }
 }
