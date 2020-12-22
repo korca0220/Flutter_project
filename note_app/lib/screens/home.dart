2f6e9fa092b91f8be4ca36b73d2debd4dd5eaa6b
Junewoo <korca02220@naver.com>
2020-12-22 23:35:15 +0900

    UI/ 수정 https://github.com/korca0220/Flutter_project/issues/2

--- note_app/lib/screens/home.dart
+++ note_app/lib/screens/home.dart
@@ -22,14 +22,18 @@ class _MyHomePageState extends State<MyHomePage> {
       body:Column(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left:20, top: 20, bottom: 20),
-            child: Text(
-              '메모메모',
-              style: TextStyle(
-                  fontSize: 36,
-                  color: Colors.blue
+            padding: EdgeInsets.only(left:20, top: 40, bottom: 20),
+            child: Container(
+              child : Text(
+                  '메모메모',
+                  style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue
+                  )
               ),
-            ),
+              alignment: Alignment.centerLeft,
+            )
           ),
           Expanded(child: memoBuilder())
         ],
@@ -47,12 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-  List<Widget> LoadMemo() {
-    List<Widget> memoList = [];
-    memoList.add(Container(color: Colors.redAccent, height: 120,));
-    memoList.add(Container(color: Colors.orange, height: 120,));
-    return memoList;
-  }
 
   Future<List<Memo>> loadMemo() async {
     DBHelper sd = DBHelper();
@@ -63,25 +61,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget memoBuilder() {
     return FutureBuilder(
         builder: (context, snap) {
-          if (snap.data.isEmpty) {
+          if (snap?.data?.isEmpty ?? true) {
             return Container(
+              alignment: Alignment.center,
               child: Text(
-                "메모를 지금 바로 추가해보세요!"
+                "지금 바로 '메모 추가' 버튼을 눌러\n 새로운 메모를 추가해보세요!!\n\n\n\n\n\n\n",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey
+                ),
               ),
             );
           }
           return ListView.builder(
+            physics: BouncingScrollPhysics(),
               itemCount: snap.data.length,
               itemBuilder: (context, index) {
                 Memo memo = snap.data[index];
-                return Column(
-                  children: <Widget>[
-                    Text(memo.title),
-                    Text(memo.text),
-                    Text(memo.edit_time)
-                  ],
-                );
-              }
+                return Container(
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(15),
+                  alignment: Alignment.center,
+                  height: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(memo.title,
+                        style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w500
+                        )
+                      ),
+                      Text(memo.text,
+                        style: TextStyle(
+                          fontSize: 15
+                        ),),
+                      Text("최종 수정 시간 : " + memo.edit_time.split('.')[0],
+                        style: TextStyle(
+                          fontSize: 12
+                        ),
+                        textAlign: TextAlign.end,
+                      )
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.blue,
+                      width: 1
+                    ),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+              );
+            }
           );
         },
         future: loadMemo(),
