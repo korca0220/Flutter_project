6c554b4b593f3176e217b24533525ebbe1aded5c
korca0220 <korca02220@naver.com>
2020-12-29 16:00:16 +0900

    CrossAxisAlignment:stretch, 컨테이너 행 사이사이 공간넣기 (SizedBox) https://github.com/korca0220/Flutter_project/issues/12

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,8 +14,10 @@ class MyApp extends StatelessWidget {
             child: Column(
           // 위아래로 정렬 ( ex) 전체를 꽉차게하고 가운데 사이에 정렬)
           // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
           // 어느 자식 기준으로 방향 정렬할지 ( ex) 가장 긴 자식의 끝부분에 정렬)
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
           // 시작 하는 부분 설정
           // verticalDirection: VerticalDirection.up,
           children: <Widget>[
@@ -24,6 +26,9 @@ class MyApp extends StatelessWidget {
               width: 100,
               color: Colors.white,
               child: Text('Container 1'),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Container(
               height: 100,
