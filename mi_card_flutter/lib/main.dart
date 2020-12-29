d7bd15f3bb863fda750818bd2654b77e5b156b04
korca0220 <korca02220@naver.com>
2020-12-29 15:57:02 +0900

    Container 정렬  https://github.com/korca0220/Flutter_project/issues/12

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,15 +11,38 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.only(left: 30),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text('Hello'),
-          ),
-        ),
+            child: Column(
+          // 위아래로 정렬 ( ex) 전체를 꽉차게하고 가운데 사이에 정렬)
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // 어느 자식 기준으로 방향 정렬할지 ( ex) 가장 긴 자식의 끝부분에 정렬)
+          crossAxisAlignment: CrossAxisAlignment.end,
+          // 시작 하는 부분 설정
+          // verticalDirection: VerticalDirection.up,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+            Container(
+              // 오른쪽 기준으로 맞추기 위한 임의의 컨테이너
+              width: double.infinity,
+            )
+          ],
+        )),
       ),
     );
   }
