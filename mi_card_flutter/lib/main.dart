94d8e46f343c946b21bd1b72f755542689232b62
korca0220 <korca02220@naver.com>
2020-12-29 16:51:52 +0900

    Layout challenge https://github.com/korca0220/Flutter_project/issues/13

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,29 +13,29 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
             child: Row(
           // 위아래로 정렬 ( ex) 전체를 꽉차게하고 가운데 사이에 정렬)
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-          // 어느 자식 기준으로 방향 정렬할지 ( ex) 가장 긴 자식의 끝부분에 정렬)
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           // 시작 하는 부분 설정
           // verticalDirection: VerticalDirection.up,
           children: <Widget>[
             Container(
-              width: 30,
-              color: Colors.white,
-              child: Text('Container 1'),
+              width: 100,
+              color: Colors.red,
             ),
-            SizedBox(
-              width: 30,
-            ),
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(100, 255, 255, 0),
+              ),
+            ]),
             Container(
               color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            Container(
-              color: Colors.red,
-              child: Text('Container 3'),
+              width: 100,
             ),
           ],
         )),
