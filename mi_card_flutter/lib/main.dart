c8f2aac66ecc1999f94fd099f3b4001152767be5
korca0220 <korca02220@naver.com>
2020-12-29 17:14:28 +0900

    Clear code https://github.com/korca0220/Flutter_project/issues/14

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,33 +11,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          // 위아래로 정렬 ( ex) 전체를 꽉차게하고 가운데 사이에 정렬)
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // 시작 하는 부분 설정
-          // verticalDirection: VerticalDirection.up,
-          children: <Widget>[
-            Container(
-              width: 100,
-              color: Colors.red,
-            ),
-            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Color.fromARGB(100, 255, 255, 0),
-              ),
-            ]),
-            Container(
-              color: Colors.blue,
-              width: 100,
-            ),
-          ],
+            child: Column(
+          children: <Widget>[],
         )),
       ),
     );
