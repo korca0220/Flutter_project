6d49c82cbb2b0e30c64070567d9b338689d18436
Junewoo <korca02220@naver.com>
2021-01-04 03:09:31 +0900

    버튼 클릭 시 프린트 https://github.com/korca0220/Flutter_project/issues/17

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(
-      MaterialApp(
-        home: MyApp(),
-      ),
-    );
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[400],
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          print('i got clicked');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
   }
 }
