50743be032feb20f56d574127125dbe511cd7f62
Junewoo <korca02220@naver.com>
2021-01-05 01:26:17 +0900

    버튼 추가, 플레이 함수 추가  https://github.com/korca0220/Flutter_project/issues/21

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,21 +4,55 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(String audio) {
+    final player = AudioCache();
+    player.play('$audio');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Center(
-              child: FlatButton(
+          child: Column(
+            children: <Widget>[
+              FlatButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
+                  playSound('note1.wav');
                 },
-                child: Text('Click me'),
+                color: Colors.red,
               ),
-            ),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note2.wav');
+                  },
+                  color: Colors.orange),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note3.wav');
+                  },
+                  color: Colors.yellow),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note4.wav');
+                  },
+                  color: Colors.green),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note5.wav');
+                  },
+                  color: Colors.teal),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note6.wav');
+                  },
+                  color: Colors.blue),
+              FlatButton(
+                  onPressed: () {
+                    playSound('note7.wav');
+                  },
+                  color: Colors.purple),
+            ],
           ),
         ),
       ),
