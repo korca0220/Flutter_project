b2da3f9e7b9ed12b4601854a57817cfab99d7ce5
Junewoo <korca02220@naver.com>
2021-01-05 00:53:21 +0900

    audioplayers package 사용하여 샘플 오디오 실행 https://github.com/korca0220/Flutter_project/issues/22

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,15 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Container(),
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Click me'),
+              ),
+            ),
           ),
         ),
       ),
