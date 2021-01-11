5e3e45122430c8ffcefb3f6416d452fc10c427ee
korca0220 <korca02220@naver.com>
2021-01-11 19:03:29 +0900

    city screen 에서 navigator value 전달 할 수 있도록 추가 https://github.com/korca0220/Flutter_project/issues/35
    - TextField 추가 ( const 모듈에 style 값 추가 )

--- Clima-Flutter/lib/screens/city_screen.dart
+++ Clima-Flutter/lib/screens/city_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:clima/utilities/constants.dart';
+
+import '../utilities/constants.dart';
 
 class CityScreen extends StatefulWidget {
   @override
@@ -7,6 +10,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +29,9 @@ class _CityScreenState extends State<CityScreen> {
               Align(
                 alignment: Alignment.topLeft,
                 child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                   child: Icon(
                     Icons.arrow_back_ios,
                     size: 50.0,
@@ -33,10 +40,22 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFiledInputDecoration,
+                  onChanged: (value) {
+                    setState(() {
+                      cityName = value;
+                    });
+                  },
+                ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
