35e1187b303f5b008996357c6dc8d0bb2ca25034
Junewoo <korca02220@naver.com>
2021-01-11 23:30:56 +0900

    dropdown menu 추가 https://github.com/korca0220/Flutter_project/issues/36

--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker/coin_data.dart';
+
+import 'coin_data.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,18 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrentcy = 'USD';
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      dropdownItems.add(
+        DropdownMenuItem(child: Text(currency), value: currency),
+      );
+    }
+    return dropdownItems;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +58,15 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrentcy,
+              items: getDropdownItems(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrentcy = value;
+                });
+              },
+            ),
           ),
         ],
       ),
