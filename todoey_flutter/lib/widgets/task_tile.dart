c714bdac195748c8bc57df89fb1cbff35dd22844
Junewoo <korca02220@naver.com>
2021-01-15 00:48:25 +0900

    ListView builder 방식으로 수정 https://github.com/korca0220/Flutter_project/issues/45
    - task_title 을 콜백함수를 받아 상태 변경하도록 수정 ( stateful -> stateless)
    - task model 추가

---
+++ todoey_flutter/lib/widgets/task_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+
+  TaskTile({this.isChecked, this.taskTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        // onChanged: toggleCheckboxState,
+      ),
+    );
+  }
+}
+// (bool newValue) {
+// setState(() {
+// isChecked = newValue;
+// });
+// },
