f01f040fd4e22d30e0b3cb409e63c254599f4c81
korca0220 <korca02220@naver.com>
2021-01-18 09:59:31 +0900

    Tabbar 기본 메뉴 구성 https://github.com/korca0220/Flutter_project/issues/47

---
+++ netflex_clone/lib/widgets/bottom_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: 50,
+        color: Colors.black,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.grey,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.home),
+              child: Text(
+                'Home',
+                style: TextStyle(fontSize: 8),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.search),
+              child: Text(
+                'Search',
+                style: TextStyle(fontSize: 8),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.save_alt),
+              child: Text(
+                'Save',
+                style: TextStyle(fontSize: 8),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.list),
+              child: Text(
+                'List',
+                style: TextStyle(fontSize: 8),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
