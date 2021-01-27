8fbb4b9932c3ad41e8b0039447aaccb8812aa45c
Junewoo <korca02220@naver.com>
2021-01-28 01:00:08 +0900

    alarm off 버튼 클릭시 알람 종료 후 삭제  https://github.com/korca0220/Flutter_project/issues/52

--- alarm/lib/screens/alarm_screen.dart
+++ alarm/lib/screens/alarm_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:alarm/widgets/notification_widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:alarm/components/alarm_data.dart';
+import 'package:alarm/components/alarm.dart';
 
 class AlarmScreen extends StatefulWidget {
   @override
@@ -8,27 +12,44 @@ class AlarmScreen extends StatefulWidget {
 class _AlarmScreenState extends State<AlarmScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Alarm Off'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(150.0),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          shape: BoxShape.circle,
+    return Consumer<AlarmData>(builder: (context, alarmData, index) {
+      List<AlarmInfo> dateList = [];
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Alarm Off'),
         ),
-        child: IconButton(
-          color: Colors.white,
-          iconSize: 60.0,
-          icon: Icon(
-            Icons.alarm_off,
+        body: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(150.0),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
           ),
-          onPressed: () {},
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 60.0,
+            icon: Icon(
+              Icons.alarm_off,
+            ),
+            onPressed: () {
+              for (int i = 0; i < alarmData.alarmCount; i++) {
+                if (alarmData.alarms[i].isAlarmOn) {
+                  dateList.add(alarmData.alarms[i]);
+                }
+              }
+              dateList.sort((a, b) => a.alarmDate.compareTo(b.alarmDate));
+              print(alarmData.alarmCount);
+              var _latestTimeData = dateList[0];
+              print(_latestTimeData.alarmDate);
+              cancelNotification(_latestTimeData.alarmId);
+              alarmData.deleteAlarm(_latestTimeData);
+              print(alarmData.alarmCount);
+              Navigator.pop(context);
+            },
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
